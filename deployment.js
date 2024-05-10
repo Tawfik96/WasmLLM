fd1a4c9974d6b561ffe1ea9c2e6fdcc390cf0fc8
Tawfik96 <112944729+Tawfik96@users.noreply.github.com>
2024-05-10 21:25:46 +0300

    Update deployment.js

--- deployment.js
+++ deployment.js
@@ -1,13 +1,1 @@
-function showPreview() {
-  document.getElementById("previewBtn").disabled = true;
-  document.getElementById("deploymentBtn").disabled = false;
-  document.getElementById("previewContent").style.display = "block";
-  document.getElementById("deploymentContent").style.display = "none";
-}
 
-function showDeployment() {
-  document.getElementById("previewBtn").disabled = false;
-  document.getElementById("deploymentBtn").disabled = true;
-  document.getElementById("previewContent").style.display = "none";
-  document.getElementById("deploymentContent").style.display = "block";
-}
