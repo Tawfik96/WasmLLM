0be9f917cecfa9ac5b71b255228edd816c5467a5
Tawfik96 <112944729+Tawfik96@users.noreply.github.com>
2024-05-11 13:17:40 +0300

    Update deployment.js

--- deployment.js
+++ deployment.js
@@ -1,1 +1,19 @@
 
+var modal = document.getElementById("myModal1");
+var btn = document.getElementById("openModal1");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// // for clicking anywhere outside the boxxxx 
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
